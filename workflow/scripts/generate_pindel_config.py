167a148bcf91823bbf0cde5f720bfb0c54c8fb7c
marrip <martin.rippin@scilifelab.uu.se>
2022-01-12 16:04:32 +0100

    Feat: Add logging and make script testable

--- workflow/scripts/generate_pindel_config.py
+++ workflow/scripts/generate_pindel_config.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
+__author__ = "Martin Rippin"
+__copyright__ = "Copyright 2021, Martin Rippin"
+__email__ = "martin.rippin@igp.uu.se"
+__license__ = "GPL-3"
 
-output = snakemake.output.config
-bam = snakemake.intput.bam
-metrics = snakemake.input.metrics
-sample = snakemake.wildcards.sample
+
+import csv
+import logging
+
 
 def insertSize(file):
     # Open file
@@ -19,13 +22,26 @@ def insertSize(file):
         # Consider only lines that contain 23 items
         if len(row) == 23:
             predict.append(row)
+    metrics.close()
 
     # Generate dict for easy access
-    parameters = dict((predict[0][n], predict[1][n]) for n in range(len(predict[0])))
+    try:
+        parameters = dict(
+            (predict[0][n], predict[1][n])
+            for n in range(len(predict[0]))
+        )
+    except IndexError:
+        logging.error("Could not detect insert size in %s", file)
+        raise
 
-    # Close file and return
-    metrics.close()
-    return parameters["MEAN_INSERT_SIZE"]
+    # Return insert size
+    try:
+        parameters["MEAN_INSERT_SIZE"]
+    except KeyError:
+        logging.error("Could not detect insert size in %s", file)
+        raise
+    else:
+        return parameters["MEAN_INSERT_SIZE"]
 
 
 def writeConfigFile(output, input, insert_size, sample_id):
@@ -35,5 +51,18 @@ def writeConfigFile(output, input, insert_size, sample_id):
 
 
 # Call functions
-insert_size = insertSize(metrics)
-writeConfigFile(output, bam, insert_size, sample)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, filename=snakemake.log[0])
+    logging.info("Read %s", snakemake.input.metrics)
+    insert_size = insertSize(snakemake.input.metrics)
+    logging.info("Insert size determined to be %s", insert_size)
+    writeConfigFile(
+        snakemake.output.config,
+        snakemake.input.bam,
+        insert_size,
+        snakemake.wildcards.sample,
+    )
+    logging.info(
+        "Successfully written config file %s",
+        snakemake.output.config,
+    )
