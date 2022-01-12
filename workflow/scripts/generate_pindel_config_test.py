eac89e56a09c5bf09b0f23c95ec86805b20ea708
marrip <martin.rippin@scilifelab.uu.se>
2022-01-12 16:05:10 +0100

    Test: Add tests and test data for generate_pindel_config

--- workflow/scripts/generate_pindel_config_test.py
+++ workflow/scripts/generate_pindel_config_test.py
@@ -7,7 +7,80 @@ __email__ = "martin.rippin@igp.uu.se"
 __license__ = "GPL-3"
 
 
-def test_insertSize():
-    from generate_pindel_config import insertSize 
-    metrics = "../../.tests/integration/qc/picard_collect_multiple_metrics/HD832.HES45_T.insert_size_metrics"
-    assert insertSize(metrics) == "200"
+import io
+import os
+import unittest
+from dataclasses import dataclass
+from generate_pindel_config import insertSize, writeConfigFile
+
+
+class TestInsertSize(unittest.TestCase):
+    def test_insertSize(self):
+        @dataclass
+        class TestCase:
+            name: str
+            input: str
+            expected: str
+
+        testcases = [
+                TestCase(
+                    name="read metrics file successfully",
+                    input=".tests/generate_pindel_config.insertSize.tsv",
+                    expected="200",
+                ),
+        ]
+
+        for case in testcases:
+            actual = insertSize(case.input)
+            self.assertEqual(
+                case.expected,
+                actual,
+                "failed test '{}': expected {}, got {}".format(
+                    case.name, case.expected, actual
+                ),
+            )
+
+
+class TestWriteConfigFile(unittest.TestCase):
+    def test_writeConfigFile(self):
+        @dataclass
+        class TestCase:
+            name: str
+            output: str
+            input: str
+            insert_size: str
+            sample_id: str
+            expected: str
+
+        testcases = [
+                TestCase(
+                    name="write config file successfully",
+                    output=(
+                        ".tests/"
+                        "generate_pindel_config.writeConfigFile.actual.tsv"
+                    ),
+                    input="test.bam",
+                    insert_size="200",
+                    sample_id="test",
+                    expected=(
+                        ".tests/"
+                        "generate_pindel_config.writeConfigFile.expected.tsv"
+                    ),
+                ),
+        ]
+
+        for case in testcases:
+            writeConfigFile(
+                case.output,
+                case.input,
+                case.insert_size,
+                case.sample_id
+            )
+            self.assertListEqual(
+                list(io.open(case.expected)),
+                list(io.open(case.output)),
+                "failed test '{}': files are different".format(
+                    case.name
+                ),
+            )
+            os.remove(case.output)
