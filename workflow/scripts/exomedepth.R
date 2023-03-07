179bab3da18e62a927b6b93d2daef7727ebdd0b6
Padraic Corcoran <padco251@marvin-head.cgu10.igp.uu.se>
2023-03-07 11:31:20 +0100

    feat(exomedepth): Add option to run exomedepth with hg38

--- workflow/scripts/exomedepth.R
+++ workflow/scripts/exomedepth.R
@@ -4,129 +4,121 @@
 # install.packages('ExomeDepth')
 
 library(ExomeDepth)
-library(rlang)
 
-{
-  data(genes.hg19)
+bedfile <- snakemake@input[["bedfile"]]
+my.bam <- snakemake@input[["bam"]]
+load(snakemake@params[["ref_count"]]) # load refcount_df
+
+message(paste('Exomedepth reference:', snakemake@params[["ref_count"]]))
+
+genome_version <- snakemake@config[["exomedepth_call"]][["genome_version"]]
+if (genome_version == "hg38") {
+
+  exons <-  read.csv(snakemake@input[["exons"]],
+                     header = TRUE, sep = "\t")
+  genes <-  read.csv(snakemake@input[["genes"]],
+                     header = TRUE, sep = "\t")
+
+} else  if (genome_version == "hg19") {
+
+  data(genes.hg19) # use genes and exons info packaged with ExomeDepth
   data(exons.hg19)
 
-  bedfile <- snakemake@input[["bedfile"]]
-  my.bam <- snakemake@input[["bam"]]
-  load(snakemake@input[["ref_count"]])
+  exons <- exons.hg19 # rename so it works for code below
+  gene <- genes.hg19
+  rm(exons.hg19, genes.hg19)
 
-  x = load(snakemake@input[["ref_count"]])
-  RefCount.mat = get(x)
+} else {
 
-  # Probes as bed.frame no header
-  bed.hg19 <- read.csv(bedfile, header = F, sep = "\t")
+  stop("Please specify genome_version in the config file 
+      as 'hg19' or 'hg38' for the exomedepth_call rule")
 
-  # Create counts dataframe for BAM
-  my.counts <- getBamCounts(bed.frame = bed.hg19, bam.files = my.bam, include.chr = F)
+}
 
-  ExomeCount.dafr <- as(my.counts[, colnames(my.counts)], "data.frame")  #Samma??
+# Probes as bed.frame no header
+targets_bed <- read.csv(bedfile, header = FALSE, sep = "\t")
 
-  if (names(ExomeCount.dafr[1])=="space"){
-  	names(ExomeCount.dafr)[1] <- "chromosome"
-  }
-  if (names(ExomeCount.dafr[5])=="names"){
-    names(ExomeCount.dafr)[5] <- "exon"
-  }
+# Create counts dataframe from the BAM file
+ExomeCount.dafr  <- getBamCounts(bed.frame = targets_bed, bam.files = my.bam,
+                                 include.chr = FALSE)
+save.image('test.Rdata')
+## Check that the ExomeCount.dafr and reference df are ordered exactly the same
+## by comparing chromosome, start and end. Coordinates are 1-based in both
+if (!all.equal(ExomeCount.dafr[,1:3], refcount_df[, 1:3])) {
+  stop("The reference used has a differenct sort order to the test sample")
+}
 
-  # Remove chr from chromosome column
-  ExomeCount.dafr$chromosome <- gsub(as.character(ExomeCount.dafr$chromosome),
-    pattern = "chr", replacement = "")
+# covert the loaded reference count df to a matrix
+RefCount.mat <- as.matrix(
+  refcount_df[, grep(names(refcount_df), pattern = ".bam$")])
 
-  # Prepare the main matrix of read count data
-  ExomeCount.mat <- as.matrix(ExomeCount.dafr[, grep(names(ExomeCount.dafr), pattern = ".bam$")])  #bara antal counts
+# Remove chr from chromosome column
+ExomeCount.dafr$chromosome <- gsub(as.character(ExomeCount.dafr$chromosome),
+  pattern = "chr", replacement = "")
 
-  # Create the aggregate reference set for this sample
-  my.choice <- select.reference.set(test.counts = ExomeCount.mat[, 1], reference.counts = RefCount.mat,
-    bin.length = (ExomeCount.dafr$end - ExomeCount.dafr$start)/1000, n.bins.reduced = 10000)
+# Prepare the main matrix of read count data
+ExomeCount.mat <- as.matrix(ExomeCount.dafr[, grep(names(ExomeCount.dafr),
+ pattern = ".bam$")])
 
-  my.reference.selected <- apply(X = RefCount.mat[, my.choice$reference.choice,
-    drop = FALSE], MAR = 1, FUN = sum)
+# Create the aggregate reference set for this sample
+my.choice <- select.reference.set(
+  test.counts = ExomeCount.mat[, 1],
+  reference.counts = RefCount.mat,
+  bin.length = (ExomeCount.dafr$end - ExomeCount.dafr$start) / 1000,
+  n.bins.reduced = 10000)
 
-  message("Now creating the ExomeDepth object")
-  all.exons <- new("ExomeDepth", test = ExomeCount.mat[, 1], reference = my.reference.selected,
-    formula = "cbind(test, reference) ~ 1")
+my.reference.selected <- apply(
+  X = RefCount.mat[, my.choice$reference.choice, drop = FALSE],
+  MAR = 1, FUN = sum)
 
-# Call CNVs
-  all.exons <- CallCNVs(x = all.exons, transition.probability = 10^-4, chromosome = ExomeCount.dafr$chromosome,
-    start = ExomeCount.dafr$start, end = ExomeCount.dafr$end, name = ExomeCount.dafr$exon)
+message("Now creating the ExomeDepth object")
+all.exons <- new("ExomeDepth", test = ExomeCount.mat[, 1],
+                 reference = my.reference.selected,
+                 formula = "cbind(test, reference) ~ 1")
 
-  if (length(all.exons@CNV.calls) > 0) {
-    # Annotate the ExomeDepth object with Exons
-    exons.hg19.GRanges <- GenomicRanges::GRanges(seqnames = exons.hg19$chromosome,
-      IRanges::IRanges(start = exons.hg19$start, end = exons.hg19$end), names = exons.hg19$exon)
+message("Now calling CNVs")
+all.exons <- CallCNVs(x = all.exons, 
+                      transition.probability = 10^-4,
+                      chromosome = ExomeCount.dafr$chromosome,
+                      start = ExomeCount.dafr$start, end = ExomeCount.dafr$end,
+                      name = ExomeCount.dafr$exon)
 
-    all.exons <- AnnotateExtra(x = all.exons, reference.annotation = exons.hg19.GRanges,
-      min.overlap = 1e-04, column.name = "exons")
+if (length(all.exons@CNV.calls) > 0) {
+  # Annotate the ExomeDepth object with Exons
+  message("Annotating with exons")
+  exons.GRanges <- GenomicRanges::GRanges(seqnames = exons$chromosome,
+    IRanges::IRanges(start = exons$start, end = exons$end), names = exons$name)
 
-    # Annotate the ExomeDepth object with Genes
-    genes.hg19.GRanges <- GenomicRanges::GRanges(seqnames = genes.hg19$chromosome,
-      IRanges::IRanges(start = genes.hg19$start, end = genes.hg19$end), names = genes.hg19$name)
-    all.exons <- AnnotateExtra(x = all.exons, reference.annotation = genes.hg19.GRanges,
-      min.overlap = 1e-04, column.name = "gene")
+  all.exons <- AnnotateExtra(x = all.exons, 
+                             reference.annotation = exons.GRanges,
+                             min.overlap = 1e-04, column.name = "exons")
 
-    save(all.exons, file = snakemake@output[["exon"]])
+  # Annotate the ExomeDepth object with Genes
+  message('Annotating with genes')
+  genes.GRanges <- GenomicRanges::GRanges(
+    seqnames = genes$chromosome,
+    IRanges::IRanges(start = genes$start, end = genes$end),
+    names = genes$name)
 
-    # Prepare output data frame
-    output_df = data.frame(all.exons@CNV.calls[order(all.exons@CNV.calls$BF, decreasing = TRUE),])
+  all.exons <- AnnotateExtra(
+    x = all.exons,
+    reference.annotation = genes.GRanges,
+    min.overlap = 1e-04, 
+    column.name = "gene")
 
-    # Txt file
-    write.table(file = snakemake@output[["result"]], x = output_df, row.names = FALSE, sep = "\t")
+  # save the cnv calls object as Rdata file
+  save(all.exons, file = snakemake@output[["exon"]])
 
+  # Prepare output data frame
+  output_df <- data.frame(all.exons@CNV.calls[order(all.exons@CNV.calls$BF,
+                                                   decreasing = TRUE),])
 
-    # NexusSV .SV.txt file
-    nexus <- c("id", "type")
-    nexus_df = output_df[nexus]
-    nexus_df$type[nexus_df$type == "duplication"] <- "CN Gain"
-    nexus_df$type[nexus_df$type == "deletion"] <- "CN Loss"
+  # Txt file
+  write.table(file = snakemake@output[["txt"]], x = output_df,
+              row.names = FALSE, sep = "\t")
 
-    names(nexus_df) <- c("Chromosome Region", "Event")
-
-    write.table(nexus_df, file = snakemake@output[["aggregated_result"]], row.names = FALSE, quote = FALSE, sep = "\t")
-
-
-    # AED file
-    keep <- c("chromosome", "start", "end", "gene", "nexons", "reads.ratio", "type",
-      "type")
-    aed_df = output_df[keep]
-    aed_df$chromosome <- sub("^", "chr", aed_df$chromosome)
-    aed_df$type[aed_df$type == "duplication"] <- "copynumber/gain"
-    aed_df$type[aed_df$type == "deletion"] <- "copynumber/loss"
-    aed_df$type.1[aed_df$type.1 == "duplication"] <- "rgb(0,0,255)"
-    aed_df$type.1[aed_df$type.1 == "deletion"] <- "rgb(255,0,0)"
-
-    aed_df$new <- NA  # blank column
-    NewNames <- c("bio:sequence(aed:String)", "bio:start(aed:Integer)", "bio:end(aed:Integer)",
-      "aed:name(aed:String)", "bio:markerCount(aed:Integer)", "bio:state(aed:Rational)",
-      "aed:category(aed:String)", "style:color(aed:Color)", "aed:value(aed:String)")
-    names(aed_df) <- NewNames
-    aed_df <- aed_df[, c("bio:sequence(aed:String)", "bio:start(aed:Integer)", "bio:end(aed:Integer)",
-      "aed:name(aed:String)", "aed:value(aed:String)", "bio:markerCount(aed:Integer)",
-      "bio:state(aed:Rational)", "aed:category(aed:String)", "style:color(aed:Color)")]
-
-    header2 <- data.frame("", "", "", "affx:ucscGenomeVersion(aed:String)", "hg19",
-      "", "", "", "", stringsAsFactors = FALSE)
-    names(header2) <- c("bio:sequence(aed:String)", "bio:start(aed:Integer)", "bio:end(aed:Integer)",
-      "aed:name(aed:String)", "aed:value(aed:String)", "bio:markerCount(aed:Integer)",
-      "bio:state(aed:Rational)", "aed:category(aed:String)", "style:color(aed:Color)")
-    aed_df <- rbind(header2, aed_df)
-    header1 <- data.frame("", "", "", "namespace:affx(aed:URI)", "http://affymetrix.com/ontology/",
-      "", "", "", "", stringsAsFactors = FALSE)
-    names(header1) <- c("bio:sequence(aed:String)", "bio:start(aed:Integer)", "bio:end(aed:Integer)",
-      "aed:name(aed:String)", "aed:value(aed:String)", "bio:markerCount(aed:Integer)",
-      "bio:state(aed:Rational)", "aed:category(aed:String)", "style:color(aed:Color)")
-    aed_df <- rbind(header1, aed_df)
-
-    write.table(aed_df, file = snakemake@output[["aed"]], row.names = FALSE, quote = FALSE, sep = "\t")
-  } else {
-    message("No result found")
-    message(snakemake@output[["result"]])
-    writeLines("\"start.p\"\t\"end.p\"\t\"type\"\t\"nexons\"\t\"start\"\t\"end\"\t\"chromosome\"\t\"id\"\t\"BF\"\t\"reads.expected\"\t\"reads.observed\"\t\"reads.ratio\"\t\"exons\"\t\"gene\"", snakemake@output[["result"]])
-    writeLines("Chromosome\tRegion\tEvent", snakemake@output[["aggregated_result"]])
-    writeLines("", snakemake@output[["aed"]])
-    writeLines("", snakemake@output[["exon"]])
-  }
+} else {
+  message("No result found")
+  writeLines("", snakemake@output[["txt"]])
+  writeLines("", snakemake@output[["exon"]])
 }
