911422a2cd2428c574b7f398c8129f0a340577a6
jonca79 <54137490+jonca79@users.noreply.github.com>
2024-09-12 13:43:45 +0200

    fix: code improvement
    Co-authored-by: Pádraic Corcoran <padraicc@users.noreply.github.com>

--- workflow/scripts/cnvkit_vcf.py
+++ workflow/scripts/cnvkit_vcf.py
@@ -43,7 +43,7 @@ for line in seg_in:
         write_vcf_header(gatk_version, sample_name)
     else:
         chrom = columns[header_map['chromosome']]
-        if chrom.find("chr") == -1:
+        if not chrom.startswith("chr"):
             chrom = f"chr{chrom}"
         start_pos = columns[header_map['start']]
         end_pos = columns[header_map['end']]
